8ba36a67e391faba3753532092e7f2148183a078
unknown <saathanakrishnaraj@gmail.com>
2026-06-14 04:21:27 -0400

    done!

--- backend/server.js
+++ backend/server.js
@@ -44,9 +44,7 @@ app.get('/api/ping', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
